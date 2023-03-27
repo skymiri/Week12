f095644def32659b99084eeded095617c7aa2aad
skymiri <hnk0225@gmail.com>
2023-03-27 10:30:56 -0700

    addEventListener click and option for once=true

--- script/script.js
+++ script/script.js
@@ -1,17 +1,12 @@
-function populateList(myShoppingList) {
-  //get access to the ul element
-  const ulElement = document.querySelector("ul.shopping");
-  console.log(ulElement);
-  // get myShoppingList array
-  // add an li under the ul element with text equal to each item of myList
-  for (let item of myShoppingList) {
-    //make a new li document.createElement
-    //update the text of the new li
-    // append the new li to the ul element
-    const newLi = document.createElement("li");
-    newLi.textContent = item;
-    ulElement.appendChild(newLi);
-  }
+const buttonRef = document.querySelector("button");
+
+function alertUser() {
+  alert("You clicked it");
+  buttonRef.removeEventListener("click", alertUser);
 }
-let myShoppingList = ["bread", "cheese", "Greenpepper"];
-populateList(myShoppingList);
+
+buttonRef.addEventListener("click", alertUser, {
+  capture: true,
+  once: true,
+  passive: true,
+});
