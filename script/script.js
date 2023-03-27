7afb083208ad9133a091e18fc2d7b9a660ecbbe2
skymiri <hnk0225@gmail.com>
2023-03-27 10:47:49 -0700

    toggle button and change background color

--- script/script.js
+++ script/script.js
@@ -2,11 +2,30 @@ const buttonRef = document.querySelector("button");
 
 function alertUser() {
   alert("You clicked it");
-  buttonRef.removeEventListener("click", alertUser);
+  //buttonRef.removeEventListener("click", alertUser);
 }
 
-buttonRef.addEventListener("click", alertUser, {
-  capture: true,
-  once: true,
-  passive: true,
-});
+// buttonRef.addEventListener("click", alertUser, {
+//   capture: true,
+//   once: true,
+//   passive: true,
+// });
+
+function changeBgdColor() {
+  // 1. change background color to pink
+  //   document.body.style.backgroundColor = "pink";
+  document.body.classList.toggle("pinkbgd");
+}
+
+buttonRef.addEventListener("click", changeBgdColor);
+
+function changeBtnName() {
+  // 1. change button clicked and clicked me
+  if (buttonRef.textContent === "Click me") {
+    buttonRef.textContent = "Clicked!";
+  } else {
+    buttonRef.textContent = "Click me";
+  }
+}
+
+buttonRef.addEventListener("click", changeBtnName);
